36d65bf3b557d5022b71843068d157dc357af9f1
Amber Woodrow <amberwoodrow@gmail.com>
2015-01-08 08:23:42 -0700

    Initial Commit

---
+++ soda_machine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  soda_machine
+//
+//  Created by Amber Woodrow on 1/8/15.
+//  Copyright (c) 2015 Amber Woodrow. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
